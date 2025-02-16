8a60f6e10809069166ca06846c2eb52755f69eb1
Zack <zwallace0790@gmail.com>
2025-02-15 20:34:46 -0800

    Using Dosuku to populate game board

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Image, StyleSheet, Platform, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
@@ -8,6 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 
 function PlayBoard() {
   const [activeCell, setActiveCell] = useState([-1, -1])
+  const [solution, setSolution] = useState([[]])
   const [gameBoard, setGameBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,6 +20,18 @@ function PlayBoard() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ])
+
+  useEffect(() => {
+    const fetchBoard = async () => {
+      const data = await fetch('https://sudoku-api.vercel.app/api/dosuku')
+      const json = await data.json()
+
+      setGameBoard(json.newboard.grids[0].value)
+      setSolution(json.newboard.grids[0].solution)
+    }
+
+    fetchBoard().catch(console.error)
+  }, [])
 
   function updateCell(newNumber: number, cellCoordinates: number[]) {
     if (
@@ -34,43 +47,63 @@ function PlayBoard() {
       return
     }
 
-    const newGameBoard = [...gameBoard]
     const [targetRow, targetValue] = cellCoordinates
+
+    if (gameBoard[targetRow][targetValue] !== 0) {
+      // Cell is populated
+      return
+    }
+
+    const newGameBoard = [...gameBoard]
 
     newGameBoard[targetRow][targetValue] = newNumber
     setGameBoard(newGameBoard)
   }
 
   return (
-    <>
+    <div>
       <div style={styles.board}>
         {gameBoard.map((row, rowNumber) => (
-          <div style={styles.boardRow}>
+          <div
+            key={rowNumber}
+            style={styles.boardRow}
+          >
             {row.map((cellValue, columnNumber) => (
-              <div style={styles.boardCell} onClick={() => setActiveCell([rowNumber, columnNumber])}>
+              <div
+                key={`${rowNumber}-${columnNumber}`}
+                style={{
+                  borderBottomWidth: 1,
+                  borderBottomColor: 'white',
+                  borderStyle: 'solid',
+                  padding: 20,
+                  backgroundColor: (activeCell[0] === rowNumber && activeCell[1] === columnNumber) ? 'blue' : 'transparent'
+                }}
+                onClick={() => setActiveCell([rowNumber, columnNumber])}
+              >
                 <ThemedText>{cellValue === 0 ? ' ' : cellValue}</ThemedText>
               </div>
             ))}
           </div>
         ))}
       </div>
-      <div>
-        {Array.from({length: 9}, (_, i) => i + 1).map(number => (
+      <div style={styles.numberBoard}>
+        {Array.from({ length: 9 }, (_, i) => i + 1).map(number => (
           <Button
+            key={`button-${number}`}
             title={number.toString()}
             onPress={() => updateCell(number, activeCell)}
           />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
 export default function HomeScreen() {
   return (
-      <ThemedView style={styles.titleContainer}>
-        <PlayBoard />
-      </ThemedView>
+    <ThemedView>
+      <PlayBoard />
+    </ThemedView>
   );
 }
 
@@ -90,20 +123,15 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     padding: 20
   },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  boardContainer: {
+    display: 'flex',
+    flexDirection: 'column'
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  numberBoard: {
+    display: 'flex',
+    gap: '10px'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  numberBoardButton: {
+    padding: 20
+  }
 });
