d84a5aec4ccaa5d1daa72ef521643431c1af7c9c
Zack <zwallace0790@gmail.com>
2025-03-22 15:50:47 -0700

    Better styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,167 @@
-import { useCallback, useEffect, useState } from '@lynx-js/react'
+import { useEffect } from '@lynx-js/react'
+import { create } from 'zustand'
 
 import './App.css'
-import arrow from './assets/arrow.png'
-import lynxLogo from './assets/lynx-logo.png'
-import reactLynxLogo from './assets/react-logo.png'
+import { Sudoku } from './utils/sudoku.js'
+
+interface GameState {
+  board: number[][]
+  solution: number[][]
+  selectedCell: number[]
+  isNewGame: boolean
+  history: number[][]
+  isWon: boolean
+  validateWin: () => void
+  undo: () => void
+  setCellValue: (newNumber: number, cellCoordinates: number[]) => void
+  setSelectedCell: (cellCoordinates: number[]) => void
+  setBoard: (newBoard: number[][]) => void
+  setSolution: (solutionBoard: number[][]) => void
+  setIsNewGame: (isNewGame: boolean) => void
+}
+
+const useGameStore = create<GameState>()(
+  (set, get) => ({
+    board: [],
+    solution: [],
+    selectedCell: [-1, -1],
+    isNewGame: true,
+    history: [],
+    isWon: false,
+    setBoard: (newBoard) => set(() => ({ board: newBoard })),
+    setIsNewGame: (isNewGame) => set(() => ({ isNewGame })),
+    setSolution: (solutionBoard) => set(() => ({ solution: solutionBoard })),
+    validateWin: () => {
+      const gameBoard = get().board
+      const solutionBoard = get().solution
+
+      for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+          if (gameBoard[row][col] !== solutionBoard[row][col]) {
+            return
+          }
+        }
+      }
+
+      set(() => ({ isWon: true }))
+    },
+    undo: () => {
+      const historyStack = get().history
+      const gameBoard = get().board
+
+      if (historyStack.length <= 0) {
+        return
+      }
+
+      const [row, col] = historyStack.pop() as number[]
+      const newGameBoard = [...gameBoard]
+
+      newGameBoard[row][col] = 0
+      set(() => ({ board: newGameBoard, history: historyStack }))
+    },
+    setCellValue: (newNumber, cellCoordinates) => {
+      if (
+        (cellCoordinates[0] < 0 || cellCoordinates[0] > 9) ||
+        (cellCoordinates[1] < 0 || cellCoordinates[1] > 9)
+      ) {
+        // Invalid coordinates
+        return
+      }
+
+      if (newNumber < 0 || newNumber > 9) {
+        // Invalid input
+        return
+      }
+
+      const [targetRow, targetColumn] = cellCoordinates
+      const gameBoard = get().board
+
+      if (gameBoard[targetRow][targetColumn] !== 0) {
+        // Cell is populated
+        return
+      }
+
+      const historyStack = get().history
+      const newGameBoard = [...gameBoard]
+
+      historyStack.push(cellCoordinates)
+      newGameBoard[targetRow][targetColumn] = newNumber
+      set(() => ({ board: newGameBoard, history: historyStack }))
+    },
+    setSelectedCell: (cellCoordinates) => {
+      const [row, col] = cellCoordinates
+
+      if ((row < 0 || row > 9) || (col < 0 || col > 9)) {
+        return
+      }
+
+      set(() => ({ selectedCell: cellCoordinates }))
+    }
+  })
+)
+
+function modInverse(a: number, m: number) {
+  a = ((a % m) + m) % m;
+  
+  // Find using Extended Euclidean Algorithm
+  for (let x = 1; x < m; x++) {
+    if ((a * x) % m === 1) {
+      return x;
+    }
+  }
+  
+  return null; // No modular inverse exists if a and m are not coprime
+}
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false)
+  const board = useGameStore((state) => state.board)
+  const setBoard = useGameStore((state) => state.setBoard)
+  const setSolution = useGameStore((state) => state.setSolution)
+  const setCellValue = useGameStore((state) => state.setCellValue)
+  const selectedCell = useGameStore((state) => state.selectedCell)
+  const setSelectedCell = useGameStore((state) => state.setSelectedCell)
+  const isNewGame = useGameStore((state) => state.isNewGame)
+  const setIsNewGame = useGameStore((state) => state.setIsNewGame)
+  const undo = useGameStore((state) => state.undo)
+  const validateWin = useGameStore((state) => state.validateWin)
+  const isWon = useGameStore((state) => state.isWon)
+  const verticalSubgridDividerCells = [2,11,20,29,38,47,56,65,74,5,14,23,32,41,50,59,68,77]
+  const horizontalSubgridDividerCells = [18,19,20,21,22,23,24,25,26,45,46,47,48,49,50,51,52,53]
 
   useEffect(() => {
-    console.info('Hello, ReactLynx')
+    if (isNewGame) {
+      const puzzle = new Sudoku('evil')
+      setBoard(puzzle.board)
+      setSolution(puzzle.solution)
+      setIsNewGame(false)
+    }
   }, [])
 
-  const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(!alterLogo)
-  }, [alterLogo])
-
   return (
-    <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
-          </view>
-          <text className='Title'>React</text>
-          <text className='Subtitle'>on Lynx</text>
-        </view>
-        <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
-            to see updates!
-          </text>
-        </view>
-        <view style={{ flex: 1 }}></view>
+    <view className="game-view">
+      <view className="game-grid">
+        {board.map((row, rowNumber) =>
+          row.map((cellValue, columnNumber) => {
+            const linearIndex = rowNumber * 9 + columnNumber
+            let borderStyle = ''
+
+            if (verticalSubgridDividerCells.includes(linearIndex)) {
+              borderStyle += 'border-right: 2px black;'
+            }
+
+            if (horizontalSubgridDividerCells.includes(linearIndex)) {
+              borderStyle += 'border-bottom: 2px black;'
+            }
+
+            return <text className='game-grid-cell' style={borderStyle}>{cellValue === 0 ? ' ' : cellValue.toString()}</text>
+          })
+        )}
+      </view>
+      <view className="game-inputs">
+        {Array.from({ length: 9 }, (_, i) => i + 1).map(number =>
+          <text className="game-input">{number}</text>
+        )}
+        <text className="game-input undo">↶</text>
       </view>
     </view>
   )
---
+++ src/utils/sudoku.ts
@@ -0,0 +1,145 @@
+export type SudokuDifficulty = 'easy' | 'medium' | 'hard' | 'evil'
+
+export class Sudoku {
+  board: number[][]
+  solution: number[][]
+  difficulty: SudokuDifficulty
+
+  private GIVENS = {
+    'easy': 35,
+    'medium': 28,
+    'hard': 22,
+    'evil': 17
+  }
+
+  constructor(difficulty: SudokuDifficulty) {
+    this.difficulty = difficulty
+    this.solution = this.generateFullBoard()
+    this.board = this.removeNumbers(this.solution)
+  }
+
+  /** Checks a given value is valid in a specified cell */
+  isValid(board: number[][], row: number, col: number, value: number) {
+    // Check row and column for value
+    for (let i = 0; i < 9; i++) {
+      if (board[row][i] === value || board[i][col] === value) {
+        return false
+      }
+    }
+
+    // Check local 3x3 grid for value
+    const localX = Math.floor(row / 3) * 3
+    const localY = Math.floor(col / 3) * 3
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[localX + i][localY + j] === value) {
+          return false
+        }
+      }
+    }
+
+    return true
+  }
+
+  /**
+   * Shuffle an array in-place using Fisher-Yates algorithm.
+   */
+  shuffleArray<T>(array: T[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  /** Generate complete and valid sudoku */
+  generateFullBoard() {
+    const board = Array(9).fill(null).map(() => Array(9).fill(0))
+
+    const fillBoard = () => {
+      for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+          if (board[row][col] === 0) {
+            const numbers = Array.from({ length: 9 }, (_, i) => i + 1);
+            this.shuffleArray(numbers)
+
+            for (let number of numbers) {
+              if (this.isValid(board, row, col, number)) {
+                board[row][col] = number
+
+                if (fillBoard()) {
+                  return true
+                }
+
+                board[row][col] = 0
+              }
+            }
+            return false
+          }
+        }
+      }
+      return true
+    }
+
+    fillBoard()
+    return board
+  }
+
+  /** Verifies uniqueness of solution */
+  isUnique(board: number[][]) {
+    const solutions: number[][][] = []
+
+    const findSolutions = (b: number[][]) => {
+      if (solutions.length > 1) return;
+
+      for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+          if (b[row][col] === 0) {
+            for (let number = 1; number <= 9; number++) {
+              if (this.isValid(b, row, col, number)) {
+                b[row][col] = number
+                findSolutions(b)
+                b[row][col] = 0
+              }
+            }
+            return
+          }
+        }
+      }
+
+      solutions.push(b.map(row => [...row]))
+    }
+
+    findSolutions(board)
+    return solutions.length === 1
+  }
+
+  /** Removes numbers from full board given difficulty */
+  removeNumbers(board: number[][]) {
+    const givens = this.GIVENS[this.difficulty]
+    const puzzle = board.map(row => [...row]);
+    let cells = []
+
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            cells.push([r, c])
+        }
+    }
+
+    this.shuffleArray(cells)
+
+    while(cells.length > givens) {
+      const [row, col] = cells.pop()!
+      const tmp = puzzle[row][col]
+      puzzle[row][col] = 0
+
+      const tmpPuzzle = puzzle.map(row => [...row])
+
+      if (!this.isUnique(tmpPuzzle)) {
+        puzzle[row][col] = tmp
+      }
+    }
+
+    return puzzle
+  }
+}
